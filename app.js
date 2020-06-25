a52195c2e6f36dbe2e48040937c179b455d221cf
punith <punith@gmail.com>
2020-06-25 11:24:56 +0530

    ceyumo:v9.1

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({
 
 require('dotenv').config()
 
+app.use(require('./routers/router'))
+
 if(process.env.NODE_ENV){
     console.log("env : "+process.env.NODE_ENV)
     app.use(express.static('client/build'))
@@ -25,7 +27,24 @@ if(process.env.NODE_ENV){
     })
 }
 
-app.use(require('./routers/router'))
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    // Pass to next layer of middleware
+    next();
+    });
 
 app.listen(port,()=>{
     console.log('Server started at port : '+port)
